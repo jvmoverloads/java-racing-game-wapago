83f88b4cf186b3924756be3f75362171f2394cbe
Kim Chanho <beroso20th@gmail.com>
2023-05-09 10:22:44 +0900

    refactor,test : @ParameterizedTest, @ValueSource 활용하여 테스트 메소드 리팩토링

--- src/test/java/com/racing/model/CarsTest.java
+++ src/test/java/com/racing/model/CarsTest.java
@@ -1,7 +1,7 @@
 package com.racing.model;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import racing.exception.InvalidCarNamesException;
 import racing.model.Cars;
 
@@ -9,32 +9,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CarsTest {
 
-    @Test
-    @DisplayName("자동차이름은 쉼표로 구분되어야 함")
-    void test1() {
-        String invalidCarNames = "kane";
-
-        assertThrows(InvalidCarNamesException.class, () -> new Cars(invalidCarNames));
-    }
-
-    @Test
-    @DisplayName("자동차이름은 쉼표로 시작할 수 없습니다")
-    void test2() {
-        String invalidCarNames = ",wevnk,vwien";
-        assertThrows(InvalidCarNamesException.class, () -> new Cars(invalidCarNames));
-    }
-
-    @Test
-    @DisplayName("자동차이름은 쉼표로 끝날 수 없습니다")
-    void test3() {
-        String invalidCarNames = "vsdvd,vewvr,nikn,";
-        assertThrows(InvalidCarNamesException.class, () -> new Cars(invalidCarNames));
-    }
-
-    @Test
-    @DisplayName("자동차이름은 최소 2대를 입력해야 합니다.")
-    void test4() {
-        String invalidCarNames = "vwet";
+    @ParameterizedTest
+    @ValueSource(strings = {"kane", ",wevnk,vwien", "vsdvd,vewvr,nikn,", "vwet"})
+    void carsTest(String invalidCarNames) {
         assertThrows(InvalidCarNamesException.class, () -> new Cars(invalidCarNames));
     }
 }
